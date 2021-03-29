d919f33414a064dd2b8013d1bd79bedfface30cf
Bruno Henrique de Paula <orec247@gmail.com>
2021-03-29 15:04:44 -0300

    Update ex008-remake.js
    Finalizei o ex 008

---
+++ Aula 11/ex008-remake.js
@@ -0,0 +1,13 @@
+var vel = 60
+
+console.log('O console funcionou corretamente')
+//console.log(`A velocidade do seu carro é: ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Você passou da velocidade permitida de 60Km/h. Sua velocidade é de ${vel}!`)
+    console.log(`MULTADO!`)
+}
+else {
+    console.log(`Boa Viagem!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
