64693f135e65445e177f3bf15f8e0387f5ec3c4e
Bruno Henrique de Paula <orec247@gmail.com>
2021-03-30 20:05:38 -0300

    Terminei ex 11
    Terminei o código em JavaScript no ex 11

--- Aula 12/ex011.js
+++ Aula 12/ex011.js
@@ -1,8 +1,9 @@
-var idade = 22
+var idade = 67
 
-if (idade < 18) {
-    console.log('Menor de idade!');
-}
-else {
-    console.log('Maior de idade!')
+if (idade < 16) {
+    console.log(`Com ${idade} anos ainda não pode vota!`);
+} else if (idade >= 16 && idade < 18 || idade >= 65){
+    console.log(`Com ${idade} anos o voto é opicional!`);
+} else {
+        console.log(`Com ${idade} anos o voto é obrigatório!`);
 }
