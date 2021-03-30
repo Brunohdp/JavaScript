a67a4f384930a850122c3a214d333ea59603ed6e
Bruno Henrique de Paula <orec247@gmail.com>
2021-03-30 20:16:27 -0300

    Update ex012.js
    Termieni o ex 12

---
+++ Aula 12/ex012.js
@@ -0,0 +1,11 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas!`)
+
+if (hora < 12 && hora >= 5) {
+    console.log(`Bom Dia, flor do dia!`)
+}   else if (hora >= 12 && hora < 19) {
+    console.log(`Boa Tarde!`)
+} else {
+    console.log(`Boa noite!`)
+}
