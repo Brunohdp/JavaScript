c1351fe6b17150fac037eae83fd509632a3d5e06
Bruno Henrique de Paula <orec247@gmail.com>
2021-03-31 13:22:47 -0300

    HDD
    Terminei o exercício da hora do dia

--- Exercícios/horaDoDia/script.js
+++ Exercícios/horaDoDia/script.js
@@ -1,1 +1,23 @@
-//alert('Olá!')
+function carregar(){
+    var msg = window.document.querySelector('div#msg')
+    var img = window.document.querySelector('img#imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 5
+
+    msg.innerHTML = `Agora são ${hora} horas!`
+    if ( hora >= 0 && hora < 12){
+        //bom dia
+        img.src = 'imagens/fotoManha.png'
+        window.document.body.style.backgroundColor = '#e4b37d'
+    } else if (hora < 19){
+        //boa tarde
+        img.src = 'imagens/fotoTarde.png'
+        window.document.body.style.backgroundColor = '#bc8467'
+    } else {
+        //boa noite
+        img.src = 'imagens/fotoNoite.png'
+        window.document.body.style.backgroundColor = '#1d3a60'
+    }
+
+}
