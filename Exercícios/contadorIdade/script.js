438060f5e5ab71cabbb24d2587a79c0cf857773d
Bruno Henrique de Paula <orec247@gmail.com>
2021-03-31 14:09:31 -0300

    fotos e html
    Adicionei todas as fotos e suas edições, juntamente com as respectivas pastas que serão utilizadas para o ex de verificador/contador de idade

--- Exercícios/contadorIdade/script.js
+++ Exercícios/contadorIdade/script.js
@@ -1,1 +1,9 @@
-//alert('Olá!')
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var anoNasc = document.querySelector('input#an.value')
+    var sexo = document.querySelector('sexo')
+    var msg = document.querySelector('msg')
+    var idade = ano - anoNasc
+    msg.innerHTML(idade)
+}
